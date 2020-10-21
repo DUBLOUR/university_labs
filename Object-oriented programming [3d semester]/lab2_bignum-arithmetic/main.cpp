3d2d816719bf4957a1f9c6fef04c939163fa3929
= <=>
2020-10-21 03:00:43 +0300

    cleaning debug

--- Object-oriented programming [3d semester]/lab2_bignum-arithmetic/main.cpp
+++ Object-oriented programming [3d semester]/lab2_bignum-arithmetic/main.cpp
@@ -1,3 +1,5 @@
+// Has implemented Karatsuba and Toom3 multiplication
+
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long LL;
@@ -8,15 +10,14 @@ class LongInt
 {
 private:
 public:
-    const static int base_size = 1; // Prefer be <= 9
-    const static LL base = 10; // Must be 10^base_size
+    const static int base_size = 4; // Prefer be <= 9
+    const static LL base = 10000; // Must be 10^base_size
     static LongInt (*mult_method)(LongInt, LongInt);
     static bool (*prime_method)(LongInt);
 
     bool is_pos;
     vector<LL> v;
     
-
     void normalize() {
         while (v.size() > 1 && !v.back())
             v.pop_back();
@@ -243,7 +244,7 @@ public:
             swap(x, y);
         assert(!x.v.empty());
         if (x.v.size() == 1) 
-            return y * (x.is_pos ? x.v[0] : -x.v[0]);
+            return y * int(x.is_pos ? x.v[0] : -x.v[0]);
 
         bool sign = x.is_pos == y.is_pos;
         x.is_pos = true; 
@@ -333,11 +334,6 @@ std::istream& operator>>(std::istream &in, LongInt& x) {
 }
 
 
-    // return a * x^2 + b * x + c
-LongInt f(LongInt& a, LongInt& b, LongInt& c, int x) {
-    if (!x) return c;
-    return a*(x*x) + b*x + c;
-}
 
 class Multiplex
 {
@@ -400,102 +396,17 @@ public:
         return res;
     }
 
+
+        // return a * x^2 + b * x + c
+    LongInt f(LongInt& a, LongInt& b, LongInt& c, int x) {
+        if (!x) return c;
+        return a*(x*x) + b*x + c;
+    }
+
         // Toom–Cook, O(n^1.46)
     static LongInt Toom3(LongInt x, LongInt y) // 0 <= x <= y
     {
-        static bool is_first = true; bool debg = is_first; is_first = false;
-        
-        int dv = (max(x.v.size(), y.v.size())+3)/3; // 1
-        // cout << " ::: " << dv << '\n';
-        LongInt m0, m1, m2, p, p_0, p_1, p_m1, p_m2, p_inf;
-        m0 = LongInt(x.v, 0, dv);  
-        m1 = LongInt(x.v, dv, dv*2);
-        m2 = LongInt(x.v, dv*2, dv*3);
-        // if (debg || 1) cout << " ---------\n" << m2 << ' ' << m1 << ' ' << m0 << "\n\n"; // 0 1 1
-
-        
-        p = m0 + m2; // 1
-        p_0 = m0;   // 1
-        p_1 = p + m1; // 2
-        p_m1 = p - m1; // 0
-        p_m2 = (p_m1 + m2)*2 - m0; // -1
-        p_inf = m2; // 0
-
-        // p_0 = m0;   
-        // p_1 = m0 + m1 + m2; 
-        // p_m1 = m0 - m1 + m2;
-        // p_m2 = m0 - m1*2 + m2*4;
-        // p_inf = m2; 
-
-
-        LongInt n0, n1, n2, q, q_0, q_1, q_m1, q_m2, q_inf;
-        n0 = LongInt(y.v, 0, dv);
-        n1 = LongInt(y.v, dv, dv*2);
-        n2 = LongInt(y.v, dv*2, dv*3);
-        // if (debg || 1) cout << " ---------\n" << n2 << ' ' << n1 << ' ' << n0 << "\n\n"; // 0 2 1
-
-        q = n0 + n2; // 1
-        q_0 = n0;   // 0
-        q_1 = q + n1; // 3
-        q_m1 = q - n1; // -1
-        q_m2 = (q_m1 + n2)*2 - n0; // -3
-        q_inf = n2; // 0
-
-        // q_0 = n0;   
-        // q_1 = n0 + n1 + n2; 
-        // q_m1 = n0 - n1 + n2;
-        // q_m2 = n0 - n1*2 + n2*4;
-        // q_inf = n2; 
-
-
-        LongInt::mult_method = &Multiplex::Karatsuba;
-        // LongInt::mult_method = &Multiplex::Toom3;
-        LongInt r_0, r_1, r_m1, r_m2, r_inf;
-        r_0 = p_0 * q_0; // 0
-        // cout << "-->>" << p_0 << ' ' << q_0 << ' ' << r_0 << '\n';
-        r_1 = p_1 * q_1; // 6
-        r_m1 = p_m1 * q_m1; // 0
-        // cout << "-->> " << p_m1 << ' ' << q_m1 << ' ' << r_m1 << '\n';
-        LongInt::mult_method = &Multiplex::Karatsuba;
-        r_m2 = p_m2 * q_m2; // 3
-        r_inf = p_inf * q_inf; // 0
-        LongInt::mult_method = &Multiplex::Toom3;
-
-        LongInt s0, s1, s2, s3, s4;
-        // if (debg) cout << "________\n";
-        s0 = r_0; // 0
-        // if (debg) cout << s0 << "\n";
-        s4 = r_inf; // 0
-        // if (debg) cout << s4 << "\n";
-        s3 = (r_m2 - r_1) / 3; // -1
-        // if (debg) cout << s3 << "\n";
-        s1 = (r_1 - r_m1) / 2; // 3
-        // if (debg) cout << s1 << "\n";
-        s2 = r_m1 - r_0; // 0
-        // if (debg) cout << s2 << "\n";
-        s3 = (s2 - s3)/2 + r_inf*2; // (0 - -1)/2 + 0*2 = 0 !!  // 0.5
-        // if (debg) cout << s3 << "\n";
-        s2 = s2 + s1 - s4; // 0 - 3 - 0 = -3
-        // if (debg) cout << s2 << "\n";
-        s1 = s1 - s3; // 3 - 0  
-        // if (debg) cout << s1 << "\n";
-
-        LongInt res = s0;
-        res += s1.shift(dv*1);
-        res += s2.shift(dv*2);
-        res += s3.shift(dv*3);
-        res += s4.shift(dv*4);
-        res.normalize();
-        return res;
-    }
-
-
-    static LongInt Toom3_byKnuth(LongInt x, LongInt y) // 0 <= x <= y
-    {
-        if (x.v.size() == 1) 
-            return y * (x.is_pos ? x.v[0] : -x.v[0]);
-        // cout << " >> " << x << ' ' << y << '\n';
-        int dv = max(x.v.size(), y.v.size())/3+1; // +1 !!!!!!!!!!!!
+        int dv = max(x.v.size(), y.v.size())/3+1; 
         LongInt m0, m1, m2, n0, n1, n2;
         m0 = LongInt(x.v, 0, dv);  
         m1 = LongInt(x.v, dv, dv*2);
@@ -504,39 +415,19 @@ public:
         n0 = LongInt(y.v, 0, dv);
         n1 = LongInt(y.v, dv, dv*2);
         n2 = LongInt(y.v, dv*2, dv*3);
-
-        // m2=4;m1=13;m0=2; n2=9;n1=2;n0=5;
         
         vector<LongInt> r(5);
-        LongInt::mult_method = &Multiplex::Karatsuba;
-        for (int i=0; i<5; ++i) 
-            r[i] = f(m2, m1, m0, i) * f(n2, n1, n0, i);
-        LongInt::mult_method = &Multiplex::Toom3_byKnuth    ;
-
+        r[0] = m0 * n0;
+        for (int i=1; i<5; ++i) 
+            r[i] = Multiplex().f(m2, m1, m0, i) * Multiplex().f(n2, n1, n0, i);
+        
         vector<vector<LongInt>> s(5);
         s[0] = r;
         for (int i=1; i<5; ++i) {
             s[i].resize(5-i);
-            for (int j=0; j<5-i; ++j) {
+            for (int j=0; j<5-i; ++j) 
                 s[i][j] = (s[i-1][j+1] - s[i-1][j]) / i;
-                cout << i << ' ' << j << '\t' << (s[i-1][j+1] - s[i-1][j]) - (((s[i-1][j+1] - s[i-1][j])/i)*i) << '\n';
-            }
         }
-
-        cout << m2 << ' ' << m1 << ' ' << m0 << '\n';
-        cout << n2 << ' ' << n1 << ' ' << n0 << '\n';
-        cout << '\n';
-
-        for (int i=0; i<5; ++i) 
-            cout << '\t' << r[i];
-        cout << "\n\n";
-
-        for (int i=0; i<5; ++i) {
-            for (int j=0; j<5-i; ++j)
-                cout << '\t' << s[i][j];
-            cout << '\n';
-        }
-
 
         vector<vector<LongInt>> ss(4);
         for (int i=0; i<4; ++i) 
@@ -550,27 +441,13 @@ public:
             for (int j=1; j<=i; ++j)
                 ss[i][j] = ss[i-1][j] - (ss[i-1][j-1] * (4-i));
 
-        for (int i=0; i<4; ++i) {
-            for (int j=0; j<5; ++j)
-                cout << '\t' << ss[i][j];
-            cout << '\n';
-        }
-
         LongInt res = ss[3][4];
         for (int i=1; i<=4; ++i)
             res += ss[3][4-i].shift(dv*i);
-        // res += ss[4][3].shift(dv*1);
-        // res += ss[4][2].shift(dv*2);
-        // res += ss[4][1].shift(dv*3);
-        // res += ss[4][0].shift(dv*4);
         res.normalize();
         return res;
     }
 
-// 
-// 
-// 
-// 
 };
 
 
@@ -611,7 +488,6 @@ int main()
 {
     LongInt::mult_method = &Multiplex::Karatsuba;
     LongInt::mult_method = &Multiplex::Toom3;
-    LongInt::mult_method = &Multiplex::Toom3_byKnuth    ;
 
     
     LongInt x,y;
@@ -633,7 +509,7 @@ int main()
     // cout << "1219326312467611632493760095208585886175176\n";
     // return 0;
     // cout << LongInt("-193633512041332459504923348015") << '\n';
-    cout << 234*234 << '\n';
+    // cout << 234*234 << '\n';
     cout << LongInt("-193633512041332459504923348015") + LongInt(0) << '\n';
     // cout << (x*y / 5)*5 << '\n';
     // cout << (x*y / 3)*3 << '\n';
